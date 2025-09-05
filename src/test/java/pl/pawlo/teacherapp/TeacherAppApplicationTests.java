0288be85d6ba72a48d307dc8f9ecc0a02278d407
Pawel <pawel98sibiga@gmail.com>
2025-09-05 22:02:53 +0200

    package update

---
+++ src/test/java/pl/pawlo/teacherapp/TeacherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pawlo.teacherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
